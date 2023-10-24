744cae67fb9e5fb2d8345de6e73b69d67aaf8a4b
Hasib24 <hasibislam1488@gmail.com>
2023-10-25 03:02:20 +0600

    calculate sqr root

---
+++ Programs/CalculateSquareRoot.py
@@ -0,0 +1,12 @@
+# ============= Solution 01 =============
+# print("Program Name : Calculate Square Root")
+# num = float(input("Enter a number :"))
+# squareRoot = num**(1/2)
+# print("Square Root : ", squareRoot)
+
+# ============= Solution 02 =============
+import math
+print("Program Name : Calculate Square Root")
+num = float(input("Enter a number :"))
+squareRoot = math.sqrt(num)
+print("Square Root : ", squareRoot)
