3e398b21e935c019e4fe79486b753b6d1a7576e4
achintya-dev-repo <achintyasolutionscom@gmail.com>
2023-10-26 00:11:27 +0600

    swap two variables

---
+++ Programs/SwapTwoVariables.py
@@ -0,0 +1,14 @@
+# ============= Solution 01 =============
+# x = 5
+# y = 7
+# temp = x
+# x = y
+# y = temp
+# print("x is ", x, "y is ", y)
+
+# ============= Solution 02 =============
+x = 5
+y = 7
+x,y =y ,x
+
+print("x is ", x, "y is ", y)
